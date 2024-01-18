0acfa24a5c4006afcf0e2304c12e545bbea24a43
evprice <elprice11@gmail.com>
2024-01-18 01:43:19 -0800

    Updated pihole disable call and script url pointer in index.html

---
+++ README.md
@@ -0,0 +1,1 @@
+Use `rye` next time
--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from subprocess import run
-
+import os
 from flask import Flask, jsonify, request
 from flask_cors import cross_origin
 
@@ -12,6 +11,6 @@ def disable():
     # return {k:v for (k,v) in request.args.items()}
     duration = int(request.args.get("duration", 0))  # should be 5 or 60
     # print(duration)
-    output = run("pwd", capture_output=True).stdout
+    os.system(f"pihole disable {duration}m")
     # maybe do something with output one day
     return jsonify(f"pihole disabled for {duration} minutes")
